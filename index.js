9fbd3c1fd480c2b157dec71bfb00260d14ff626f
Breon White <bdwhite92@gmail.com>
2022-01-23 01:32:18 -0500

    commit tree 1d9cabc6951aeebc430909e8db15ee20518b6933

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+function hasTargetSum (array, target) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      let number = array[i];
+      let numberTwo = array[j];
+      if (number + numberTwo === target) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
 
 /* 
   Write the Big O time complexity of your function here
@@ -16,7 +26,6 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +38,24 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
+
 
 module.exports = hasTargetSum;
